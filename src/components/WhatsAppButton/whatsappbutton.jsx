68ab1ef0713d9cb442a02957ab372b652c53fadc
Sri <thisissridharshanofficial@gmail.com>
2026-08-11 22:44:44 +0530

    Update catering site

--- src/components/WhatsAppButton/whatsappbutton.jsx
+++ src/components/WhatsAppButton/whatsappbutton.jsx
@@ -1,7 +1,7 @@
 import { FaWhatsapp } from "react-icons/fa";
 
 function WhatsAppButton() {
-  const phoneNumber = "917548895467";
+  const phoneNumber = "918925059589";
 
   const message =
     "வணக்கம் Nalan Catering! எனது நிகழ்விற்கு Catering Service பற்றி தகவல் வேண்டும்.";
@@ -14,6 +14,7 @@ function WhatsAppButton() {
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-5 right-5 z-50 bg-green-500 hover:bg-green-600 text-white w-14 h-14 rounded-full flex items-center justify-center shadow-xl hover:scale-110 transition"
+      aria-label="Contact Nalan Catering on WhatsApp"
     >
       <FaWhatsapp size={32} />
     </a>
